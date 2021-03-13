79040ccbba0e63179e1c1af0f8814f3efcb3cd86
Olga Gromyko <olgagromyko@MacBook-Air-Olga.local>
2021-03-13 06:58:52 +0300

    added technique's questions

--- js/testArray/pictureTestList.js
+++ js/testArray/pictureTestList.js
@@ -740,9 +740,9 @@ export default {
                 '../../pictures/imagesSociety/0/3.jpg',
                 '../../pictures/imagesSociety/0/4.jpg',
             ],
-            trueValue: 1 
+            trueValue: 1
         },
-     {
+        {
             question: 'Укажите автора теории "трёх технологических волн"?',
             values: [
                 '../../pictures/imagesSociety/1/1.jpg',
@@ -750,9 +750,9 @@ export default {
                 '../../pictures/imagesSociety/1/3.jpg',
                 '../../pictures/imagesSociety/1/4.jpg',
             ],
-            trueValue: 2 
+            trueValue: 2
         },
-     {
+        {
             question: 'Что из этого буддистский храм?',
             values: [
                 '../../pictures/imagesSociety/2/1.jpg',
@@ -760,9 +760,9 @@ export default {
                 '../../pictures/imagesSociety/2/3.jpg',
                 '../../pictures/imagesSociety/2/4.jpg',
             ],
-            trueValue: 2 
+            trueValue: 2
         },
-     {
+        {
             question: 'Выберите изображение соответствующее типу темперамента ФЛЕГМАТИК?',
             values: [
                 '../../pictures/imagesSociety/3/1.jpg',
@@ -772,7 +772,7 @@ export default {
             ],
             trueValue: 2
         },
-     {
+        {
             question: 'Какое изображение соответствует понятию НЕПОЛНАЯ СЕМЬЯ?',
             values: [
                 '../../pictures/imagesSociety/4/1.jpg',
@@ -821,7 +821,7 @@ export default {
                 '../../pictures/imagesSociety/6/4.jpg',
             ],
             trueValue: 3
-        }   
+        }
     ],
     technique: [
         {
@@ -832,7 +832,87 @@ export default {
                 '../../pictures/imagesTechnique/0/3.jpg',
                 '../../pictures/imagesTechnique/0/4.jpg',
             ],
-            trueValue: 1 
+            trueValue: 1
         },
+        {
+            question: 'Что из представленного на картинке было изобретено не в XIX веке?',
+            values: [
+                '../../pictures/imagesTechnique/1/1.jpeg',
+                '../../pictures/imagesTechnique/1/2.jpeg',
+                '../../pictures/imagesTechnique/1/3.jpeg',
+                '../../pictures/imagesTechnique/1/4.jpeg',
+            ],
+            trueValue: 3
+        },
+        {
+            question: 'На какой картинке изображена астролябия?',
+            values: [
+                '../../pictures/imagesTechnique/2/1.jpeg',
+                '../../pictures/imagesTechnique/2/2.jpeg',
+                '../../pictures/imagesTechnique/2/3.jpeg',
+                '../../pictures/imagesTechnique/2/4.jpeg',
+            ],
+            trueValue: 0
+        },
+        {
+            question: 'На какой картинке изображён сейсмограф?2',
+            values: [
+                '../../pictures/imagesTechnique/3/1.jpeg',
+                '../../pictures/imagesTechnique/3/2.jpeg',
+                '../../pictures/imagesTechnique/3/3.jpeg',
+                '../../pictures/imagesTechnique/3/4.jpg',
+            ],
+            trueValue: 1
+        },
+        {
+            question: 'В честь какого великого изобретателя Илон Маск назвал компанию?',
+            values: [
+                '../../pictures/imagesTechnique/4/1.jpeg',
+                '../../pictures/imagesTechnique/4/2.jpeg',
+                '../../pictures/imagesTechnique/4/3.jpeg',
+                '../../pictures/imagesTechnique/4/4.jpeg',
+            ],
+            trueValue: 2
+        },
+        {
+            question: 'Выберите из представленных парусников бриг',
+            values: [
+                '../../pictures/imagesTechnique/5/1.jpeg',
+                '../../pictures/imagesTechnique/5/2.jpeg',
+                '../../pictures/imagesTechnique/5/3.jpeg',
+                '../../pictures/imagesTechnique/5/4.jpeg',
+            ],
+            trueValue: 0
+        },
+        {
+            question: 'Выберите из представленных парусников шхуну',
+            values: [
+                '../../pictures/imagesTechnique/5/1.jpeg',
+                '../../pictures/imagesTechnique/5/2.jpeg',
+                '../../pictures/imagesTechnique/5/3.jpeg',
+                '../../pictures/imagesTechnique/5/4.jpeg',
+            ],
+            trueValue: 2
+        },
+        {
+            question: 'Выберите из представленных парусников галеон',
+            values: [
+                '../../pictures/imagesTechnique/5/1.jpeg',
+                '../../pictures/imagesTechnique/5/2.jpeg',
+                '../../pictures/imagesTechnique/5/3.jpeg',
+                '../../pictures/imagesTechnique/5/4.jpeg',
+            ],
+            trueValue: 1
+        },
+        {
+            question: 'Выберите из представленных парусников бригантину',
+            values: [
+                '../../pictures/imagesTechnique/5/1.jpeg',
+                '../../pictures/imagesTechnique/5/2.jpeg',
+                '../../pictures/imagesTechnique/5/3.jpeg',
+                '../../pictures/imagesTechnique/5/4.jpeg',
+            ],
+            trueValue: 3
+        }
     ]
 }
